8b1f886f382a99bf4472ca8fbc676217971d215b
Eyam Odey <messiah2007@yahoo.com>
2020-12-04 15:42:06 +0100

    first commit on git

---
+++ index.js
@@ -0,0 +1,57 @@
+
+// Automatically set the current year in the copyright section of footer
+const today = new Date();
+const siteOwner = "buildyourcapacity.org" ;
+document.getElementById("copyrightInfo").innerHTML = " ©" + today.getFullYear() + " " + siteOwner ;
+
+$('#blogCarousel').carousel({
+  interval: 5000
+})
+
+$('.carousel .blog-post').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
+// SET COUNTDOWN ON BLOG PAGE
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in an element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "GOING LIVE NOW!";
+  }
+}, 1000);
